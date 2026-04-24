d820f235dbe3eb84ae887ab07bfc82d7cc353004
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-04-24 07:52:21 -0400

    refactor: update StatsDisplay layout and improve character count handling in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { TextInput } from './components/TextInput/TextInput'
 import StatsDisplay from './components/StatsDisplay/StatsDisplay'
+console.log(React)
 
 function App() {
   const [inputValue, setInputValue] = useState('')
 
+  //created a handler function to take in a string and set that text to the new state value
   const handleInputChange= (text:string)=>{
     console.log("Input value:", text);
     setInputValue(text)
   }
-
   const statsStart = {
-     characterCount: 0,
+     characterCount: inputValue.length,
     wordCount: 0,
     readingTime: 5 
   }
 
   return (
     <>
-     <TextInput onTextChange={handleInputChange}/>
-     <StatsDisplay stats={statsStart}/>
+     <TextInput onTextChange={handleInputChange} />
+     <StatsDisplay stats={statsStart} showReadingTime={true}/>
     </>
   )
 }
--- src/components/StatsDisplay/StatsDisplay.tsx
+++ src/components/StatsDisplay/StatsDisplay.tsx
@@ -4,7 +4,7 @@ import CharacterCounter from '../CharacterCounter/CharacterCounter'
 
 const StatsDisplay: React.FC<StatsDisplayProps> =({stats, showReadingTime})=> {
   return (
-    <div className="w-full p-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 grid grid-cols-3 grid-rows-1 gap-0">
+    <div className="w-full p-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 grid grid-cols-3 grid-rows-2 gap-0">
     <div>
       <p>Characters</p>
       {stats.characterCount}
@@ -17,7 +17,7 @@ const StatsDisplay: React.FC<StatsDisplayProps> =({stats, showReadingTime})=> {
     </div>
     <div>
       <h6>Reading Time</h6>
-      {stats.readingTime}
+      {showReadingTime && <p>{stats.readingTime}</p>}
     </div>
     </div>
   )
